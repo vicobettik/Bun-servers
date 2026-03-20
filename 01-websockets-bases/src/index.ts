2c84f64bbf1b671297fd8b4f7c3b5b6f87c5c3d2
Victor Amaury Fuentes Menes <vicuo@hotmail.com>
2026-03-19 19:35:39 -0600

    creacion de primer servidor con websockets

---
+++ 01-websockets-bases/src/index.ts
@@ -0,0 +1,27 @@
+const server = Bun.serve({
+  port: 3100,
+  routes: {
+    '/': new Response('Hola mundo!!!')
+  },
+  fetch(req, server) {
+    // upgrade the request to a WebSocket
+    if (server.upgrade(req)) {
+      return; // do not return a Response
+    }
+    return new Response("Upgrade failed", { status: 500 });
+  },
+  websocket: {
+    message(ws, message) {
+      console.log({ ws, message });
+    }, // a message is received
+    open(ws) {
+      console.log("Cliente conectado");
+    }, // a socket is opened
+    close(ws, code, message) {
+      console.log("Cliente desconectado");
+    }, // a socket is closed
+    drain(ws) {}, // the socket is ready to receive more data
+  }, // handlers
+});
+
+console.log(`Esuchando puerto ${server.url}`);
